48bba4e8e77ad4aa147ce58f31be2ce9647f1f1e
Isaac <theicean@gmail.com>
2023-08-14 10:00:16 -0600

    updated data service comments

--- src/lib/orderDataService.jsx
+++ src/lib/orderDataService.jsx
@@ -2,10 +2,11 @@ import { useEffect, useState } from "react";
 export function OrderDataService() {
 	const [orders, setOrders] = useState([]);
 	function fetchOrderData() {
+		// TODO: Currently using allorgins to bypass cors issues. Good enough for a visual demo, but needs to be updated for security before PROD
 		const corsURL = "https://api.allorigins.win/raw?url=";
+
 		const dataUrl = "https://doc.strider.tech/content/receipts.json";
 
-		// const dataUrl = "./data.json";
 		return fetch(corsURL + dataUrl, {
 			method: "GET",
 			headers: {
